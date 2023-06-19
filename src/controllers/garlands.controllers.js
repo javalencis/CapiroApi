668466a3932a38801078cc8e54757b4df714ada2
Jonathan Valencia <jonathana.valencia@udea.edu.co>
2023-06-19 15:07:33 -0500

    Controladores rutas guirnaldas

--- src/controllers/garlands.controllers.js
+++ src/controllers/garlands.controllers.js
@@ -1,14 +1,27 @@
+import Garland from '../models/garland.model.js'
 
+export const findGarlands = async (req,res) =>{
+    try {     
+        const garlands = await Garland.find()
+        res.json(garlands)
+    } catch (error) {
+        console.log(error)
+    }
+}
+export const createGarland =async (req,res) =>{
+    const newGarland = new Garland(req.body)
+    const garlandSaved = await newGarland.save()
+    res.json(garlandSaved)
+}
 
-export const findGarlands = (req,res) =>{
-    res.send("Todas las guirnaldas")
+export const updateGarland =async (req,res) =>{
+    const idGarland =  req.params.id
+    const garlandUpdated = await Garland.findByIdAndUpdate(idGarland,req.body,{
+        new:true
+    })
+    res.json(garlandUpdated)
 }
-export const createGarland = (req,res) =>{
-    res.send("Guardando guirnalda")
-}
-export const updateGarland = (req,res) =>{
-    res.send("Actualizando guirnalda")
-}
-export const deleteGarland = (req,res) =>{
-    res.send("Eliminando guirnalda")
+export const deleteGarland = async(req,res) =>{
+    const garlandDelete = await Garland.findByIdAndDelete(req.params.id)
+    res.json(garlandDelete)
 }
---
+++ src/models/garland.model.js
@@ -0,0 +1,35 @@
+import {Schema,model} from 'mongoose'
+
+
+const garlandSchema = new Schema({
+    bloque:{
+        type:Number,
+        require:true,
+        trim:true
+    },
+    guirnalda:{
+        type:Number,
+        require:true,
+        trim:true
+    },
+    estado:{
+        type:String,
+        require:true,
+        trim:true
+    },
+    num_bombillas:{
+        type:Number,
+        require:true,
+        trim:true
+    },
+    umbral:{
+        type:Number,
+        require:true,
+        trim:true
+    }
+},{
+    timestamps:true
+})
+
+
+export default model('Garland',garlandSchema)
