3a5439df176afe0d4e10ce96050dfe98db30edb2
Jonathan Valencia <jonathana.valencia@udea.edu.co>
2023-06-19 21:40:13 -0500

    Dependencias mqtt y cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import garlandRoutes from './routes/garlands.routes.js'
 import alertRoutes from './routes/alerts.routes.js'
 import registerRoutes from './routes/registers.routes.js'
@@ -7,6 +8,7 @@ import registerRoutes from './routes/registers.routes.js'
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/garlands',garlandRoutes)
 app.use('/api/alerts',alertRoutes)
